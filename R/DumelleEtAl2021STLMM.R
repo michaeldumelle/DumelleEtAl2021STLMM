d74225fd3e1cf18b4aa54ac04d87c2ff7575b8b3
Michael Dumelle <mjdumelle@gmail.com>
2021-03-05 14:32:56 -0800

    updated package documentation

--- R/DumelleEtAl2021STLMM.R
+++ R/DumelleEtAl2021STLMM.R
@@ -1,6 +1,11 @@
 #' DumelleEtAl2021STLMM: Fit Spatio-Temporal Linear Mixed Models
 #'
-#' This package does stuff
+#' A Supplementary R Package to to "A Linear Mixed Model Formulation for
+#'   Spatio-Temporal Random Processes with Computational Advances for the
+#'   Product, Sum, and Product-Sum Covariances." The funtions most often
+#'   used are \code{stlmm()} for fitting spatio-temporal linear mixed
+#'   models, \code{summary()} for summarizing those models, and \code{predict}
+#'   for making predictions.
 #'
 #' @import stats
 "_PACKAGE"
